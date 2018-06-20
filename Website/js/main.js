7f66854e2155bc33d94986f71a7ec0887a7f8445
levanton <31097602+levanton@users.noreply.github.com>
2018-06-20 18:34:36 +0300

    removed download

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,13 +1,10 @@
+
 $(document).ready(function() {
   $(".burger-btn").click(function(){
       event.preventDefault();
       $("body").toggleClass("nav_open");
   });
 });
-
-
-
-
 function scrollNav() {
   $('.anchor').click(function(){  
     //Toggle Class
