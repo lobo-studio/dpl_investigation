0210f78cbf132f50a12993e91e619a373487f46a
levanton <31097602+levanton@users.noreply.github.com>
2018-06-22 13:34:32 +0300

    jekyll

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+  $(".burger-btn").click(function(){
+      event.preventDefault();
+      $("body").toggleClass("nav_open");
+  });
+});
+function scrollNav() {
+  $('.anchor').click(function(){  
+    //Toggle Class
+    var theClass = $(this).attr("class");
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 75
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
