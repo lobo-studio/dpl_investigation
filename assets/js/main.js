6b2dba91c575afc8530aeca927cbf48478ec75f6
levanton <31097602+levanton@users.noreply.github.com>
2018-07-04 11:11:02 +0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,12 +42,7 @@ var formEl = document.getElementById("contact-form");
 formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (grecaptcha) {
-    var recaptchaResponse = grecaptcha.getResponse();
-    if (!recaptchaResponse) { // reCAPTCHA not clicked yet
-      return false;
-    }
-  }
+
 
   var request = new XMLHttpRequest();
 
